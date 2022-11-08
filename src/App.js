8cce5f2ee26072f596105edc8846955f71467dbe
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-11-09 01:08:15 +0300

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-import { GlobalStyle } from './globalstyles';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import Feature from "./components/features";
+import Footer from "./components/footer";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData, productDataTwo } from "./components/Products/data";
+import { GlobalStyle } from "./globalstyles";
 
 function App() {
-  return (
-    <GlobalStyle />
-  );
+	return (
+		<Router>
+			<GlobalStyle />
+			<Hero />
+			<Products heading="Choose your favorite" data={productData} />
+			<Feature />
+			<Products heading="Sweet Treats for You" data={productDataTwo} />
+			<Footer />
+		</Router>
+	);
 }
 
 export default App;
