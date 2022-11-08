83448613b4fe8270b4f2123879a6729a5a900b82
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-11-09 01:22:38 +0300

    debugging

---
+++ src/components/footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { FooterContainer, FooterWrap, SocialIconLink, SocialIcons, SocialLogo, SocialMedia, SocialMediaWrap } from './footerelement';
+
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to='/'>Pizza</SocialLogo>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Youtube'>
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                href='//www.twitter.com/briandesignz'
+                target='_blank'
+                aria-label='Twitter'
+                rel='noopener noreferrer'
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
