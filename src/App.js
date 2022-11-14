198a5949cee2cba731288d7e5b451df2ddbd1d5f
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-11-14 15:26:40 +0300

    debugging

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Feature from "./components/features";
 import Footer from "./components/footer";
 import Hero from "./components/Hero";
 import Products from "./components/Products";
 import { productData, productDataTwo } from "./components/Products/data";
+import MenuPage from "./components/Menu";
 import { GlobalStyle } from "./globalstyles";
+import { BrowserRouter, Routes, Route } from "react-router-dom";  
+  
 
 function App() {
-	return (
-		<Router>
+	return ( 
+		<BrowserRouter>
+		<Routes>
+            <Route path="MenuPage" element={<MenuPage/>}/>
 			<GlobalStyle />
 			<Hero />
 			<Products heading="Choose me" data={productData} />
 			<Feature />
 			<Products heading="Sweet Treats for You" data={productDataTwo} />
 			<Footer />
-		</Router>
+		</Routes> 
+		</BrowserRouter >
 	);
 }
 
